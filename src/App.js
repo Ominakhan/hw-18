2b8acd1083288a3d09f278fd5d29c51776e0e63f
Omina <omina.mamatalieva.ch@gmail.com>
2023-03-04 17:42:48 +0600

    add hw-18

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Calculator from './components/Counter';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import Header from './components/Header';
+import Auth from './components/Auth';
+
+function App() {
+  return (
+    <Provider store={store}>
+          <Header/>
+          <Calculator />
+          <Auth/>
+    </Provider>
+
+  );
+}
+
+export default App;
---
+++ src/store/auth/auth.js
@@ -0,0 +1,30 @@
+
+export const authActionTypes = {
+    LOG_IN: "LOG_IN",
+    LOG_OUT: "LOG_OUT"
+}
+
+const initialState = {
+    email: "",
+    isAuthorized: false,
+}
+
+export const authReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case authActionTypes.LOG_IN:
+      return {
+        ...state,
+        email: action.payload,
+        isAuthorized: true,
+      };
+    case authActionTypes.LOG_OUT:
+        return initialState
+        // ili  {
+        //  ...state,
+        //   email: "",
+        //   isAuthorized: false,
+        //}
+    default:
+        return state;
+  }
+}
